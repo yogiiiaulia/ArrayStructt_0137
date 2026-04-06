bff1bb5bf88f7ba3f444c337a18ef652023550bd
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-06 17:15:17 +0700

    tampilkan data (membaca data array)

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -6,5 +6,8 @@ int main() {
     int aku[5] = {10,20,30,40,50};
     //deklarasi array tanpa inisialisasi
     int kamu[5];
+    //tampilkan data(membaca data array)
+    cout << " Data pada index 1 = " << aku[1] << endl;
+    // ganti index ke 1 (menulis data array)
     
 }
