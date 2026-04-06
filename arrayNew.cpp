90ef72600991a6202153bcb9dcf71843811a220a
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-06 17:19:37 +0700

    mengisi index 1 per 1

--- arrayNew.cpp
+++ arrayNew.cpp
@@ -13,4 +13,15 @@ int main() {
     cout << endl;
     cout << "Data pada index 1 = " << aku[1] << endl;
     // Mengisi Satu per Satu
+    cout << "Data index 0 = ";
+    cin >> kamu[0];
+    cout << "Data index 1 = ";
+    cin >> kamu[1];
+    cout << "Data index 2 = ";
+    cin >> kamu[2];
+    cout << "Data index 3 = ";
+    cin >> kamu[3];
+    cout << "Data index 4 = ";
+    cin >> kamu[4];
+    cout << endl;
 }
