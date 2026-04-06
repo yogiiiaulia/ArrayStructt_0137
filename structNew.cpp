184702f46a9c09baedfe5b0960d9a140506d7bad
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-06 17:47:10 +0700

    mengisi data

--- structNew.cpp
+++ structNew.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 using namespace std;
 
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main (){
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+    cin >> mhs.alamat;
+    cout << "umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    
+}
