322a1d103173b1e8086311e016e9637d66c52534
yogiiiaulia <yogiauliaardiano@gmail.com>
2026-04-06 18:30:02 +0700

    mengisi data dan menampilkan data

--- structNew2.cpp
+++ structNew2.cpp
@@ -15,3 +15,23 @@ struct Orang
     int umur;
 };
 
+int main (){
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    // tampilkan 
+    cout << "Menampilkan Data" << endl;
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Desa : " << mhs.alamat.desa << endl;
+    cout << " Kota : " << mhs.alamat.kota << endl;
+    cout << " Umur : " << mhs.umur << endl;
+}
